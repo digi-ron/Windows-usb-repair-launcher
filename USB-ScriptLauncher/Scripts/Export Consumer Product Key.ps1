13d067dd5a9377a322280370f35bafb9b37efd05
digi-ron <32566377+digi-ron@users.noreply.github.com>
2024-01-25 19:48:26 +1100

    Added Product Key Export script

---
+++ USB-ScriptLauncher/Scripts/Export Consumer Product Key.ps1
@@ -0,0 +1,11 @@
+# set variables for file output - taken from Common Repairs Script
+$fileoutPath = Split-Path $PSScriptRoot -Parent
+$fileoutPath += "/DumpOutputs"
+mkdir $fileoutPath -Force | Out-Null
+$computerinformation = Get-ComputerInfo
+$PCName = $computerinformation.CsName
+$UserName = $computerinformation.CsUserName.Split("\")[1]
+$TDStamp = Get-Date -UFormat %y%m%d%H%M%S
+$filename = "$($fileoutPath)\$($PCName)--$($UserName)--$($TDStamp)--REGPKEY.reg"
+
+reg export "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SoftwareProtectionPlatform" $filename
